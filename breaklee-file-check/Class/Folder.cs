29ff7cdcffd3601d1672d6e35b27931afef77bef
immafishball <hansmarcos14@gmail.com>
2024-10-24 10:25:31 +0800

    #1 Structured Classes Files

---
+++ breaklee-file-check/Class/Folder.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace breaklee_file_check.Class
+{
+    internal class Folder
+    {
+        public string Name { get; set; }
+
+        // Constructor to read from BinaryReader
+        public Folder(BinaryReader reader)
+        {
+            byte[] nameBytes = reader.ReadBytes(260); // 260 bytes for the name
+            Name = Encoding.ASCII.GetString(nameBytes).TrimEnd('\0');
+        }
+
+        public string FolderName()
+        {
+            return Name;
+        }
+
+        // Size of the folder structure in bytes
+        public int Size => 260; // Only name field
+    }
+}
--- breaklee-file-check/Form1.cs
+++ breaklee-file-check/Form1.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using breaklee_file_check.Class;
+using System;
 using System.Collections.Generic;
 using System.Data;
 using System.IO;
@@ -13,60 +14,6 @@ namespace breaklee_file_check
         public Form1()
         {
             InitializeComponent();
-        }
-
-        // Folder class to represent the Folder structure
-        class Folder
-        {
-            public string Name { get; set; }
-
-            // Constructor to read from BinaryReader
-            public Folder(BinaryReader reader)
-            {
-                byte[] nameBytes = reader.ReadBytes(260); // 260 bytes for the name
-                Name = Encoding.ASCII.GetString(nameBytes).TrimEnd('\0');
-            }
-
-            public string FolderName()
-            {
-                return Name;
-            }
-
-            // Size of the folder structure in bytes
-            public int Size => 260; // Only name field
-        }
-
-        // File class to represent the File structure
-        class Files
-        {
-            public byte FolderIndex { get; set; }
-            public string Name { get; set; }
-            public byte Unk1 { get; set; }
-            public string Hash { get; set; }
-            public byte Unk2 { get; set; }
-            public int Size => 1 + 60 + 1 + 32 + 1; // folder_index + name(60) + unk1 + hash(32) + unk2
-
-            // Constructor to read from BinaryReader
-            public Files(BinaryReader reader)
-            {
-                FolderIndex = reader.ReadByte();          // 1 byte for folder_index
-                byte[] nameBytes = reader.ReadBytes(60);  // 60 bytes for the name
-                Name = Encoding.ASCII.GetString(nameBytes).TrimEnd('\0');
-                Unk1 = reader.ReadByte();                 // 1 byte for unk1
-                byte[] hashBytes = reader.ReadBytes(32);  // 32 bytes for the hash
-                Hash = Encoding.ASCII.GetString(hashBytes).TrimEnd('\0');
-                Unk2 = reader.ReadByte();                 // 1 byte for unk2
-            }
-
-            public string FileNameRead()
-            {
-                return FolderIndex == 0 ? "MagicKey" : Name;
-            }
-
-            public void FileHashWrite(string hashValue)
-            {
-                Hash = hashValue;
-            }
         }
 
         private void button1_Click(object sender, EventArgs e)
